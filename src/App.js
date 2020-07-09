fdfc117f973e89c786ed3c073833ba07a12837e6
linchen5913 <bushigi5913@gmail.com>
2020-07-09 14:38:41 +0800

    adding shop page and some other preview pictures

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'; 
 import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shopPage/shop';
 import './App.css';
 
-const HatsPage = () =>(
-  <div>
-    <h1>Hats Page!!</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
