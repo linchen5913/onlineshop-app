d84c8a299980e5302dfac4458d21ad730b86f33c
linchen5913 <bushigi5913@gmail.com>
2020-07-14 15:05:25 +0800

    adding dropdown list and toggle feature

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import './header.styles.scss';
 //currentUser comes from reducer
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser,hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             {/*SVG is a high performance image yet its size is really tiny*/}
@@ -21,18 +23,22 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/contact'>
                 CONTACT
             </Link>
-            {
-                currentUser ?
-                    (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
-                    :
-                    (<Link className='option' to='/signin'>SIGN IN</Link>)
-            }
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            ) : (
+                <Link className='option' to='/signin'>SIGN IN</Link>
+            )}
+            <CartIcon />
         </div>
+        {
+            hidden ? null : (<CartDropdown />)
+        }
     </div>
 );
 //this "state" comes from rootReducer, from combinedReducer(to be specific)
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
