2b6e471cbdb043fa99232d7c23dfe030ec1ebeab
linchen5913 <bushigi5913@gmail.com>
2020-07-16 14:59:21 +0800

    updating stripe intergration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51H5PnfL3tqwDrzEgrcHNQWmTPTWZGBqYlKWIfndpi1FyT4IkwrOwOssSS0DFXLW3hhidxtSNBBbZE5zDgxFJyKxP0052RQ6Wgr';
+
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment successful!! Thank you for your purchase.');
+    }
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name={`Lin Chen's Online-Shop`}
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
