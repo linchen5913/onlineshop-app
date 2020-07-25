30a3ddbba32fe3cd587614f2140ba45594870fc0
linchen5913 <bushigi5913@gmail.com>
2020-07-25 15:46:05 +0800

    added app.use compression

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }))
+    app.use(compression);
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
